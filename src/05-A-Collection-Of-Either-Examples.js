447f52c56bb82e7593c0973edd63db97e75cea6c
Kevin Sullivan <wolverine.ks@gmail.com>
2017-07-30 00:49:52 -0700

    05, part 1

--- src/05-A-Collection-Of-Either-Examples.js
+++ src/05-A-Collection-Of-Either-Examples.js
@@ -1,4 +1,4 @@
-import { fromNullable } from './utils.js'
+import { Right, fromNullable } from './utils.js'
 
 // export const openSite = (currentUser) => {
 //   if (currentUser) {
@@ -11,7 +11,6 @@ import { fromNullable } from './utils.js'
 export const openSite = (currentUser) =>
   fromNullable(currentUser)
     .fold(showLogin, renderPage)
-
 const showLogin = () => 'Please login'
 const renderPage = (currentUser) => `Hello ${currentUser.username}`
 
@@ -24,8 +23,34 @@ const renderPage = (currentUser) => `Hello ${currentUser.username}`
 // }
 
 export const getPrefs = (user) =>
-  fromNullable(user.premium)
+  new Right(user)
+    .chain(getPremiumStatus)
     .fold(loadDefaultPrefs, () => loadUserPrefs(user))
-
+const getPremiumStatus = (user) => fromNullable(user.premium)
 const loadDefaultPrefs = () => 'DEFAULT_PREFERENCES'
 const loadUserPrefs = (user) => user.preferences
+
+// export const getStreetName = (user) => {
+//   const address = user.address
+//
+//   if (address) {
+//     const street = address.street
+//
+//     if (street) {
+//       return street.name
+//     }
+//   }
+//   return 'no street'
+// }
+
+export const getStreetName = (user) =>
+  new Right(user)
+    .chain(getAddress)
+    .chain(getStreet)
+    .map(getName)
+    .fold(onError, onSuccess)
+const getAddress = (user) => fromNullable(user.address)
+const getStreet = (address) => fromNullable(address.street)
+const getName = (street) => street.name
+const onError = (_e) => 'no street'
+const onSuccess = (name) => name
--- test/05-A-Collection-Of-Either-Examples.spec.js
+++ test/05-A-Collection-Of-Either-Examples.spec.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
 import * as Examples from '../src/05-A-Collection-Of-Either-Examples.js'
-const { openSite, getPrefs } = Examples
+const { openSite, getPrefs, getStreetName } = Examples
 
-describe('openSite', () => {
+describe('openSite', function () {
   describe('when user is logged in', function () {
     it('should return the login page', function () {
       const currentUser = { username: 'John123' }
@@ -25,7 +25,7 @@ describe('openSite', () => {
   })
 })
 
-describe('getPrefs', () => {
+describe('getPrefs', function () {
   describe('when user is premium', function () {
     it('should return user preferences', function () {
       const currentUser = { username: 'John123', premium: true, preferences: true }
@@ -41,6 +41,43 @@ describe('getPrefs', () => {
       const currentUser = { username: 'John123', premium: undefined, preferences: true }
       const expected = 'DEFAULT_PREFERENCES'
       const actual = getPrefs(currentUser)
+
+      expect(actual).to.equal(expected)
+    })
+  })
+})
+
+describe('getStreetName', function () {
+  describe('onError', function () {
+    describe('when address is undefined', function () {
+      it('should return \'no street\' message', function () {
+        const user = { username: 'John', address: undefined }
+        const expected = 'no street'
+        const actual = getStreetName(user)
+
+        expect(actual).to.equal(expected)
+      })
+    })
+
+    describe('when street is undefined', function () {
+      it('should return \'no street\' message', function () {
+        const user = { username: 'John', address: { street: undefined } }
+        const expected = 'no street'
+        const actual = getStreetName(user)
+
+        expect(actual).to.equal(expected)
+      })
+    })
+  })
+
+  describe('onSuccess', function () {
+    it('should return the street name', function () {
+      const user = {
+        username: 'John',
+        address: { street: { name: 'Main St.' } }
+      }
+      const expected = 'Main St.'
+      const actual = getStreetName(user)
 
       expect(actual).to.equal(expected)
     })
